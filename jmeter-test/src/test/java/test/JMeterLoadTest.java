c389afad12af537507c8b65b87dbd27c03d86f45
Oleksandr Kyetov <oleksandrkyetov@cloud.upwork.com>
2018-10-06 17:17:12 +0300

    Split REST application and JMeter test

--- jmeter-test/src/test/java/test/JMeterLoadTest.java
+++ jmeter-test/src/test/java/test/JMeterLoadTest.java
@@ -4,38 +4,17 @@ import org.apache.jmeter.engine.StandardJMeterEngine;
 import org.apache.jmeter.save.SaveService;
 import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jorphan.collections.HashTree;
-import org.assertj.core.api.Assertions;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
 
 import java.io.File;
 import java.net.URL;
 
 @RunWith(JUnit4.class)
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-public class ApplicationTest {
+public class JMeterLoadTest {
 
     private static final String JMETER_HOME = "/home/olkyupwork/Softwares/ApacheJMeter";
-
-    @LocalServerPort
-    private int port;
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Test
-    @Ignore
-    public void verifyServerIsUpAndRespond() {
-        Assertions.assertThat(restTemplate.getForEntity("http://localhost:" + port + "/ping", String.class).getStatusCode())
-            .isIn(HttpStatus.OK, HttpStatus.BAD_REQUEST, HttpStatus.NOT_FOUND);
-    }
 
     @Test
     public void jmeterTest() throws Exception {
@@ -51,7 +30,7 @@ public class ApplicationTest {
         SaveService.loadProperties();
 
         // Load existing test plan from .jmx
-        final URL url = ApplicationTest.class.getClassLoader().getResource("test-rest-application-test-plan.jmx");
+        final URL url = JMeterLoadTest.class.getClassLoader().getResource("test-rest-application-test-plan.jmx");
         final HashTree testPlanTree = SaveService.loadTree(new File(url.toURI()));
 
         // Run JMeter Test
